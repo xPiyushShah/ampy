be5a04a9cd1bc9c7772adc8431d569bbdb81eec8
Piyush Kumar shah <86467579+xPiyushShah@users.noreply.github.com>
2024-07-24 07:08:36 +0530

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,30 +11,30 @@ class Home extends Controller
 
     public function __construct()
     {
-        $this->db = \Config\Database::connect(); // Default connection, no need to specify "test" here
+        $this->db = \Config\Database::connect(); 
         $this->datatable = $this->db->table('tabledata');
     }
 
     public function index()
     {
-        // Load views
+      
         return view('template/header') . view('index') . view('template/footer');
     }
 
     public function add()
     {
-        // Load add page view
+        
         return view('addpage');
     }
 
     public function addData()
     {
-        // Insert data into 'table'
+       
         $data = $this->request->getPost();
 
         $this->db->table('table')->insert($data);
 
-        // Console log confirmation
+        
         echo "Data inserted successfully.";
     }
 }
